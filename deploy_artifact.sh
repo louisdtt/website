c192f640a9f2bbf79faf12915971df7b8a648c6d
louis <louis@debian>
2023-06-17 14:27:42 +0200

    node server

--- deploy_artifact.sh
+++
@@ -1,2 +0,0 @@
-docker build -t europe-west1-docker.pkg.dev/workspace-388218/artifacts/my-website .
-docker push europe-west1-docker.pkg.dev/workspace-388218/artifacts/my-website
---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000; 
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('*', (req, res) => {
+  res.redirect('/');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});

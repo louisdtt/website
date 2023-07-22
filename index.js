6ba3deb47ba2ad16635a264e98a9ee232273c72d
louis <louis@debian>
2023-07-22 17:19:37 +0200

    rm images + added mobile zoom

--- index.js
+++ index.js
@@ -4,14 +4,20 @@ const path = require('path');
 const app = express();
 const port = 3000; 
 
+// Render index.html on /
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
+// Access resources in the public folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Redirect everything to /
 app.get('*', (req, res) => {
   res.redirect('/');
 });
 
+// Open app on port 3000
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
